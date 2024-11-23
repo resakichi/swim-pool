ae6c85ce4eb5643cdffaf091f46e60ab3e98c88a
toby_collins <toby_collins@MacBook-Air.local>
2024-11-23 21:08:19 +0500

    initialize project

---
+++ src/main/java/com/swimpool/swim/pool/SwimPoolApplication.java
@@ -0,0 +1,13 @@
+package com.swimpool.swim.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swimpool/swim/pool/SwimPoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swimpool.swim.pool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimPoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
